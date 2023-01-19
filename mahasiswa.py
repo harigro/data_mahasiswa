c1e320e9fe2a53328d15bdc268d40ba98391dd75
Yuhari <trierbank@gmail.com>
2023-01-20 00:20:44 +0700

    memperbaiki algortima

--- mahasiswa.py
+++ mahasiswa.py
@@ -1,31 +1,15 @@
 from sys import exit
-import json
+from crud_json import setDataJsonFile, getDataJsonFile
+import json, pprint
 
 
 def addNewData() -> dict:
     nama = input("Masukan Nama : ")
     asal = input("Masukan Asal : ")
-    return {"Nama":nama, "Asal":asal}         
-
-# json buat, baca, bahrui, hapus data
-def buat_json(mahasiswa):
-    data = json.dumps(mahasiswa, indent=4)
-    with open("sample.json", "w") as f:
-	    f.write(data)
-
-def baca_json():
-    with open('sample.json', 'r') as openfile:
-	    data = json.load(openfile)
-    return data
-
-def update():
-    pass
-
-def hapus():
-    pass
-
+    return {"Nama":nama, "Asal":asal}
 
 def main(mahasiswa: dict):
+    pp = pprint.PrettyPrinter(indent=4, sort_dicts=False)
     while True:
         print("\n1. Buat")
         print("2. Tampil")
@@ -39,19 +23,15 @@ def main(mahasiswa: dict):
                 mahasiswa[n] = umum
             else:
                 print("Data tersedia")
-            buat_json(mahasiswa)
+            setDataJsonFile(mahasiswa, "db.json")
         if p == 2:
-            n = input("Masukan NIM : ")
-            if n in mahasiswa:
-                print(mahasiswa[n])
-            else:
-                print("Data tidak tersedia")
+            pp.pprint(getDataJsonFile("db.json"))
         if p == 3:
-            print(mahasiswa)
+            pp.pprint(getDataJsonFile("db.json"))
             n = input("Masukan NIM : ")
             if n in mahasiswa:
                 del mahasiswa[n]
-                buat_json(mahasiswa)
+                setDataJsonFile(mahasiswa, "db.json")
                 print("Data terhapus")
             else:
                 print("Data tidak tersedia")
